9d4000a9a9a86291a733a76de9cb0b6a2e5537e8
infinisri06-sri <infinisri06@gmail.com>
2025-10-21 18:46:57 +0530

    comments
    Refactored area and circle classes for clarity and added comments.

--- 8.cpp
+++ 8.cpp
@@ -1,30 +1,43 @@
 #include<iostream>
 using namespace std;
-class area{
-    public:
-        void calculate(int a){
-            cout<<"The Area Of Square: "<<a*a<<endl;
-        }
-        void calculate(int l,int b){
-            cout<<"The Area of Rectangle: "<<l*b<<endl;
-        }
-        virtual void display(){
-            cout<<"These All are box"<<endl;
+
+// Base class: area
+class area {
+public:
+    // Overloaded function to calculate area of a square
+    void calculate(int a) {
+        cout << "The Area Of Square: " << a * a << endl;
+    }
+
+    // Overloaded function to calculate area of a rectangle
+    void calculate(int l, int b) {
+        cout << "The Area of Rectangle: " << l * b << endl;
+    }
+
+    // Virtual function to allow runtime polymorphism
+    virtual void display() {
+        cout << "These All are box" << endl;
     }
 };
-class circle:public area{
-    public:
-        void display() override{
-            cout<<"This is Circle"<<endl;
-        }
-};
-int main(){
-    area A;
-    A.calculate(6);
-    A.calculate(8,2);
 
-    circle c;      //base class pointer derived class object
-    area *a;
-    a=&c;
-    a->display();
+// Derived class: circle inherits from area
+class circle : public area {
+public:
+    // Override the base class display method
+    void display() override {
+        cout << "This is Circle" << endl;
+    }
+};
+
+int main() {
+    area A;              // Create object of base class
+    A.calculate(6);      
+    A.calculate(8, 2);   
+
+    circle c;            // Create object of derived class
+    area *a;             
+    a = &c;              
+
+    a->display();        
+    return 0;
 }

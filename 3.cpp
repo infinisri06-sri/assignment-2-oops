437d59ec1a7515c0148c7c5764f1baf309ace560
infinisri06-sri <infinisri06@gmail.com>
2025-10-21 18:04:56 +0530

    Refactor code with enhanced comments
    Updated comments for clarity and improved code readability.

--- 3.cpp
+++ 3.cpp
@@ -1,24 +1,26 @@
 #include<iostream>
 using namespace std;
-class Shape{
+class Shape{ // create a class shape 
     private:
-        int length;
+        int length;   
         int breadth;
     public:
-        Shape(int l,int b){
+        Shape(int l,int b){ // create a constructor to initalize length and breadth 
             this->length=l;
             this->breadth=b;
         }
-        int getLength(){
+        int getLength(){  // getter function for length
             return length; 
         }
-        int getBreadth() {
+        int getBreadth() { // / getter function for breadth
             return breadth; 
         }
 };
-class Rectangle:public Shape{
+class Rectangle:public Shape{   //  Derived class Rectangle inherits from Shape
+
     public:
-        Rectangle(int len,int br):Shape(len,br){
+        Rectangle(int len,int br):Shape(len,br){// Constructor passes dimensions to base class Shape
+
         }
         int area(){
             return getLength()*getBreadth();
@@ -32,8 +34,9 @@ int main(){
     int l,b;
     cout<<"Enter the Length and Breadth :"<<endl;
     cin>>l>>b;
-    Rectangle r(l,b);
+    Rectangle r(l,b);  // create Rectangle object with user input
     cout<<"The Area of Rectangle :"<<r.area()<<endl;
     cout<<"The perimeter of Rectangle :"<<r.perimeter()<<endl;
+
 
 }
